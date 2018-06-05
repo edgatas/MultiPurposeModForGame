70c880fe5ea97f7ac7d81bc74c909d032d779eb0
Edgaras Celkys <edgar.celkys@gmail.com>
2018-06-05 18:40:08 +0100

    Commands and ingame output place

---
+++ SearchQuestions/Commands.cs
@@ -0,0 +1,34 @@
+﻿using InSimDotNet;
+using InSimDotNet.Packets;
+
+namespace SearchQuestions
+{
+    class Commands
+    {
+        public Commands()
+        {
+
+        }
+
+        public void RCM_SetMessage(InSim _inSim, string text)
+        {
+            _inSim.Send(
+                new IS_MST { Msg = "/rcm " + text, ReqI = 1 }
+            );
+        }
+
+        public void RCM_ShowAll(InSim _inSim)
+        {
+            _inSim.Send(
+                new IS_MST { Msg = "/rcm_all", ReqI = 1 }
+            );
+        }
+
+        public void RCC_RemoveAll(InSim _inSim)
+        {
+            _inSim.Send(
+                new IS_MST { Msg = "/rcc_all", ReqI = 1 }
+            );
+        }
+    }
+}
