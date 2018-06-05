02b0cddab97a5b60cfb7e46a51dc581e22ee773d
Edgaras Celkys <edgar.celkys@gmail.com>
2018-06-05 18:09:35 +0100

    Color codes. Could be useful.

---
+++ SearchQuestions/Enums/LFSColors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchQuestions.Enums
+{
+    public static class LFSColors
+    {
+        public const string BLACK = "^0";
+        public const string RED = "^1";
+        public const string GREEN = "^2";
+        public const string YELLOW = "^3";
+        public const string BLUE = "^4";
+        public const string MAGENTA = "^5";
+        public const string CYAN = "^6";
+        public const string WHITE = "^7";
+        public const string GREY = "^8";
+    }
+}
