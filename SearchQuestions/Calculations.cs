fa459eab4fa394e22d50f82f494c37c20eb0ed42
Edgaras Celkys <edgar.celkys@gmail.com>
2018-06-06 22:13:36 +0100

    Distance calculations with 2 coordinates

--- SearchQuestions/Calculations.cs
+++ SearchQuestions/Calculations.cs
@@ -1,4 +1,7 @@
-﻿namespace SearchQuestions
+﻿using System.Collections.Generic;
+using System;
+
+namespace SearchQuestions
 {
     class Calculations
     {
@@ -7,6 +10,22 @@
 
         }
 
+        private int DistanceBetweenCars(Car car1, Car car2)
+        {
+            return (int)Math.Sqrt(((car1.X - car2.X) * (car1.X - car2.X) +
+                        (car1.Y - car2.Y) * (car1.Y - car2.Y) +
+                        (car1.Z - car2.Z) * (car1.Z - car2.Z)));
+        }
 
+        public int[] GetDistanesToCars(List<Car> allCars, Car currentCar)
+        {
+            int length = allCars.Count;
+            int[] distances = new int[length];
+            for (int index = 0; index < length; index++)
+            {
+                distances[index] = DistanceBetweenCars(allCars[index], currentCar);
+            }
+            return distances;
+        }
     }
 }
