faefba84c60cbb481d4dc61af28713f37482ac98
Edgaras Celkys <edgar.celkys@gmail.com>
2018-06-09 22:35:20 +0100

    New Function (Closest from list) Type change

--- SearchQuestions/Calculations.cs
+++ SearchQuestions/Calculations.cs
@@ -10,22 +10,39 @@ namespace SearchQuestions
 
         }
 
-        private int DistanceBetweenCars(Car car1, Car car2)
+        private UInt16 DistanceBetweenCars(Car car1, Car car2)
         {
-            return (int)Math.Sqrt(((car1.X - car2.X) * (car1.X - car2.X) +
+            return (UInt16)Math.Sqrt(((car1.X - car2.X) * (car1.X - car2.X) +
                         (car1.Y - car2.Y) * (car1.Y - car2.Y) +
                         (car1.Z - car2.Z) * (car1.Z - car2.Z)));
         }
 
-        public int[] GetDistanesToCars(List<Car> allCars, Car currentCar)
+        public UInt16[] GetDistanesToCars(List<Car> allCars, Car currentCar)
         {
             int length = allCars.Count;
-            int[] distances = new int[length];
+            UInt16[] distances = new UInt16[length];
             for (int index = 0; index < length; index++)
             {
                 distances[index] = DistanceBetweenCars(allCars[index], currentCar);
             }
             return distances;
+        }
+
+        public int GetClosestIndex(UInt16[] distances)
+        {
+            int length = distances.Length;
+            int distance = 10000;
+            int index = -1;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (distance > distances[i] && distances[i] != 0)
+                {
+                    distance = distances[i];
+                    index = i;
+                }
+            }
+            return index;
         }
     }
 }
