02cdcd3ab3cbb006123b92234a28b75a5b98af02
Edgaras Celkys <edgar.celkys@gmail.com>
2018-06-24 16:05:48 +0100

    Testing Layout Placement

--- SearchQuestions/Commands.cs
+++ SearchQuestions/Commands.cs
@@ -1,13 +1,27 @@
 ﻿using InSimDotNet;
 using InSimDotNet.Packets;
 
+using System.Collections.Generic;
+
 namespace SearchQuestions
 {
     class Commands
     {
+        public int objectX { get; set; }
+        public int objectY { get; set; }
+        public int objectZ { get; set; }
+        public int objectHeading { get; set; }
+        public int objectIndex { get; set; }
+        private int objectClock;
+
         public Commands()
         {
-
+            objectX = -710;
+            objectY = 950;
+            objectZ = 25;
+            objectHeading = -180;
+            objectIndex = 174;
+            objectClock = 0;
         }
 
         public void RCM_SetMessage(InSim _inSim, string text)
@@ -85,6 +99,42 @@ namespace SearchQuestions
             );
         }
 
+
+        public void create1Object(InSim _inSim)
+        {
+            if (objectClock > 3)
+            {
+                IS_AXM axmDelete = new IS_AXM
+                {
+                    PMOAction = ActionFlags.PMO_DEL_OBJECTS
+                };
+
+                ObjectInfo simpleObjectDelete = new ObjectInfo { X = (short)(objectX * 16), Y = (short)(objectY * 16), Zbyte = (byte)(objectZ * 4), Heading = (byte)objectHeading, Index = (byte)objectIndex, Flags = 0 };
+                axmDelete.Info.Add(simpleObjectDelete);
+                _inSim.Send(
+                    axmDelete
+                );
+
+                objectHeading += 1;
+
+
+                IS_AXM axmCreate = new IS_AXM
+                {
+                    PMOAction = ActionFlags.PMO_ADD_OBJECTS
+                };
+
+                ObjectInfo simpleObjectCreate = new ObjectInfo { X = (short)(objectX * 16), Y = (short)(objectY * 16), Zbyte = (byte)(objectZ * 4), Heading = (byte)objectHeading, Index = (byte)objectIndex, Flags = 0 };
+                axmCreate.Info.Add(simpleObjectCreate);
+                _inSim.Send(
+                    axmCreate
+                );
+                objectClock = 0;
+            }
+            else
+            {
+                objectClock++;
+            }
+        }
 
     }
 }
