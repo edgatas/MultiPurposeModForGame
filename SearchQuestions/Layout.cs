ff1aa631bb6d8c029845762a8447309773c3fc2f
Edgaras Celkys <edgar.celkys@gmail.com>
2018-07-06 19:10:01 +0100

    New class for layouts

---
+++ SearchQuestions/Layout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchQuestions
+{
+    class Layout
+    {
+        public int objectX { get; set; }
+        public int objectY { get; set; }
+        public int objectZ { get; set; }
+        public int objectHeading { get; set; }
+        public int objectIndex { get; set; }
+
+        public Layout(int X, int Y, int Z, int heading, int index)
+        {
+            objectX = X;
+            objectY = Y;
+            objectZ = Z;
+            objectHeading = heading;
+            objectIndex = index;
+        }
+    }
+}
